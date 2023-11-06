64ee6ce447b60d00af3cc04ac8960ee1a224c6a9
siyam79 <siyamahmed3827@gmail.com>
2023-11-06 19:33:42 +0600

    Data base mongo DB and client site  added a server Site

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express')
+require("dotenv").config();
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 const cors = require('cors')
-
 
 // middleware 
 app.use(express.json());
 app.use(cors())
 
 
-const uri = "mongodb+srv://job-search:cXUGofrfq6RXCKWy@cluster0.29n9zox.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.29n9zox.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,7 +31,7 @@ async function run() {
 
 
 
-        //  data base data add 
+        //  data base job data add 
 
         app.post('/addJob', async (req, res) => {
             const job = req.body;
@@ -39,13 +40,42 @@ async function run() {
             res.send(result)
         })
 
-        //  add data get 
+        // all job add data get 
         app.get("/getJob", async (req, res) => {
             const cursor = jobCollection.find();
             const result = await cursor.toArray()
             res.send(result)
         })
     
+
+        //  single data gate 
+
+        app.get("/job/:id", async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const quaery = {
+                _id: new ObjectId(id),
+            };
+            const result = await jobCollection.findOne(quaery)
+            res.send(result)
+        })
+
+
+
+        // app.get("/job/:id", async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log(id);
+        //     const quaery = {
+        //         _id: new ObjectId(id),
+        //     };
+        //     const result = await jobCollection.findOne(quaery)
+        //     res.send(result)
+        // })
+
+
+
+
+
 
 
 
@@ -65,10 +95,8 @@ run().catch(console.dir);
 
 
 
-
-
 app.get('/', (req, res) => {
-    res.send('Hello World! job owner')
+    res.send('Hello World! job owner Your well Come ')
 })
 
 app.listen(port, () => {
